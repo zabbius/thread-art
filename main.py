d7f41dbfdfb62634f4e909760a84c0407d3f27d5
zabbius <zabbius@gmail.com>
2021-03-11 16:10:42 +0300

    weight function and autocontrast

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from math import sin, cos
 import skimage.draw
 import skimage.io
 import skimage.transform
+import skimage.exposure
+
+import numpy
 
 
-def make_thread_art(image_path, on_next_line, pins_count=200, threshold=15, start_pin=None,
-                    max_radius=500, scale=1.0, min_radius=200):
+def make_thread_art(image_path, on_next_line, pins_count=200, threshold=25, start_pin=None,
+                    max_radius=500, scale=1.0, min_radius=200, debug=False):
     pixels = skimage.io.imread(image_path, True)
-    pixels = skimage.util.invert(pixels)
-    pixels = skimage.util.img_as_ubyte(pixels)
 
     height, width = pixels.shape
     radius = min(width, height) // 2
@@ -32,6 +33,9 @@ def make_thread_art(image_path, on_next_line, pins_count=200, threshold=15, star
     center_x = (width - 1) // 2
     center_y = (height - 1) // 2
 
+    pixels = skimage.exposure.equalize_adapthist(pixels)
+    pixels = skimage.util.img_as_ubyte(pixels)
+
     pins = []
     for n in range(pins_count):
         angle = math.pi / 2 - math.pi * 2 * n / pins_count
@@ -43,12 +47,17 @@ def make_thread_art(image_path, on_next_line, pins_count=200, threshold=15, star
     lines_set = set([(n, n) for n in range(pins_count)])
     cur_pin = start_pin
 
+    last_score = 255
+
     while True:
         def process_pin(pin):
             line = skimage.draw.line(*pins[cur_pin], *pins[pin])
-            yy, xx = line
-            avg_sum = sum(pixels[yy, xx]) // len(yy)
-            return pin, avg_sum, line
+
+            line_pixels = pixels[line]
+            avg = int(numpy.average(line_pixels))
+
+            score = 255 - avg
+            return score, pin, line
 
         try_pins = (
             pin for pin in range(pins_count)
@@ -57,17 +66,22 @@ def make_thread_art(image_path, on_next_line, pins_count=200, threshold=15, star
 
         guess = map(process_pin, try_pins)
 
-        best_pin, best_avg, best_line = max(guess, key=lambda g: g[1])
+        best_score, best_pin, best_line = max(guess, key=lambda g: g[0])
 
-        if best_avg < threshold:
+        if best_score < threshold:
             break
+
+        if debug and last_score - best_score > 10:
+            last_score = best_score
+            skimage.io.imshow(pixels)
+            skimage.io.show()
 
         lines_set.add((cur_pin, best_pin))
 
         if on_next_line:
-            on_next_line(cur_pin, best_pin)
+            on_next_line(cur_pin, best_pin, best_score)
 
-        pixels[best_line] = 0
+        pixels[best_line] = 255
 
         cur_pin = best_pin
 
@@ -93,22 +107,22 @@ class TurtlePreviewEdgeHandler:
 
         self.turtle.update()
 
-    def draw_edge(self, pin1, pin2):
+    def draw_edge(self, pin1, pin2, score):
         self.turtle.pu()
         self.turtle.goto(*self.pins[pin1 - 1])
         self.turtle.pd()
         self.turtle.goto(*self.pins[pin2 - 1])
         self.turtle.update()
 
-        print(pin1 + 1, pin2 + 1)
+        print(score, pin1 + 1, pin2 + 1)
 
     def done(self):
         self.turtle.done()
 
 
 class StdOutEdgeHandler:
-    def draw_edge(self, pin1, pin2):
-        print(pin1 + 1, pin2 + 1)
+    def draw_edge(self, pin1, pin2, score):
+        print(score, pin1 + 1, pin2 + 1)
 
     def done(self):
         pass
@@ -117,13 +131,14 @@ class StdOutEdgeHandler:
 parser = argparse.ArgumentParser()
 parser.add_argument("image", help="image file", type=str)
 parser.add_argument("-p", "--pins", default=200, help="Pins count", type=int, dest='pins_count')
-parser.add_argument("-t", "--threshold", default=20, help="Fill threshold", type=int, dest='threshold')
+parser.add_argument("-t", "--threshold", default=25, help="Fill threshold", type=int, dest='threshold')
 parser.add_argument("-s", "--start", default=None, help="Start pin", type=int, dest='start_pin')
 parser.add_argument("--max-radius", default=500, help="Max art radius", type=int, dest='max_radius')
 parser.add_argument("--min-radius", default=200, help="Min art radius", type=int, dest='min_radius')
 parser.add_argument("-x", "--scale", default=1.0, help="Image scale factor", type=float, dest='scale')
 parser.add_argument("-v", "--preview", action='store_true', help="Image scale factor", dest='preview')
 parser.add_argument("--preview-radius", default=500, help="Preview radius", type=int, dest='preview_radius')
+parser.add_argument("--debug", action='store_true', help="Debug using scikit plots", dest='debug')
 
 
 args = parser.parse_args()
@@ -138,6 +153,6 @@ else:
 
 make_thread_art(args.image, edge_handler.draw_edge,
                 pins_count=args.pins_count, threshold=args.threshold, start_pin=args.start_pin,
-                max_radius=args.max_radius, min_radius=args.min_radius, scale=args.scale)
+                max_radius=args.max_radius, min_radius=args.min_radius, scale=args.scale, debug=args.debug)
 
 edge_handler.done()
